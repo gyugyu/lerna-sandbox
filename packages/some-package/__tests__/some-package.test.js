f15377732ebf992f25e7e1660c57ff4f6ae0a3cc
Yusuke Yagyu <gyugyu@pepabo.com>
2020-03-02 16:23:45 +0900

    create scss and ignore dist directory

--- packages/some-package/__tests__/some-package.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const somePackage = require('..');
-
-describe('some-package', () => {
-    it('needs tests');
-});
--- packages/some-package/lib/some-package.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = somePackage;
-
-function somePackage() {
-    // TODO
-}
